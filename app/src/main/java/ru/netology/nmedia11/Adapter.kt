cb54fe5f7177648ad6bd82cc4f48f187d043408c
Alexey Zelenkov <alan.lapsky@gmail.com>
2022-12-11 22:50:09 +0300

    11.12.2022 - Continue homework "Fragments".

--- app/src/main/java/ru/netology/nmedia11/Adapter.kt
+++ app/src/main/java/ru/netology/nmedia11/Adapter.kt
@@ -44,6 +44,7 @@ class PostViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(post: Post) {
+        val bigClick = View.OnClickListener { onInteractionListener.onPost(post.id) }
         binding.apply {
             author.text = post.author
             published.text = post.published
@@ -102,41 +103,15 @@ class PostViewHolder(
             ibVideo.setOnClickListener {
                 onInteractionListener.onPlay(post)
             }
-            author.setOnClickListener {
-                onInteractionListener.onPost(post.id)
-            }
-            published.setOnClickListener {
-                onInteractionListener.onPost(post.id)
-            }
-            content.setOnClickListener {
-                onInteractionListener.onPost(post.id)
-            }
-            ivViews.setOnClickListener {
-                onInteractionListener.onPost(post.id)
-            }
+//            group1.setOnClickListener(bigClick)
+            icon.setOnClickListener(bigClick)
+            author.setOnClickListener(bigClick)
+            published.setOnClickListener (bigClick)
+            content.setOnClickListener(bigClick)
+            ibVideo.setOnClickListener(bigClick)
+            ivViews.setOnClickListener(bigClick)
         }
     }
-
-    // конвертер: на входе - число, на выходе строка типа "999" или "1К" или "2,2М"
-//     fun convert(num: Int): String {
-//        val form: String
-//        val n: Int
-//        when(num) {
-//            in 0 .. 999 -> return num.toString()
-//            in 1000 .. 9999 -> {
-//                n = num%1000
-//                form = (if((n < 100)||(n>900)) "%.0f" else "%.1f")
-//                return String.format(form, num.toDouble()/1000) + "K"
-//            }
-//            in 10000 .. 999999 -> return String.format("%.0f", num.toDouble()/1000) + "K"
-//            else -> {
-//                n = num%1000000
-//                form = (if((n < 100000)||(n>900000)) "%.0f" else "%.1f")
-//                return String.format(form, num.toDouble()/1000000) + "M"
-//            }
-//        }
-//    }
-
 }
 
 class PostDiffCallback: DiffUtil.ItemCallback<Post>() {
--- app/src/main/java/ru/netology/nmedia11/OnePostFragment.kt
+++ app/src/main/java/ru/netology/nmedia11/OnePostFragment.kt
@@ -1,16 +1,22 @@
 package ru.netology.nmedia11
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import ru.netology.nmedia11.EditPostFragment.Companion.textArg
 import ru.netology.nmedia11.databinding.FragmentOnePostBinding
 
-class OnePostFragment : Fragment() {
+class OnePostFragment: Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +29,12 @@ class OnePostFragment : Fragment() {
 
         viewModel.data.observe(viewLifecycleOwner) { posts ->
             val post = posts.find { it.id == postId } ?: return@observe
+
+            val playVideoListener = OnClickListener{
+                val playVideoIntent = Intent(Intent.ACTION_VIEW, Uri.parse(post.video))
+                startActivity(playVideoIntent)
+            }
+
             with(binding) {
                 author.text = post.author
                 published.text = post.published
@@ -45,9 +57,61 @@ class OnePostFragment : Fragment() {
                     val thumbnailUri = Uri.parse("https://img.youtube.com/vi/${videoId}/0.jpg")
                     Glide.with(requireParentFragment()).load(thumbnailUri).into(binding.ivThumbnail) // TODO проверить "with"
                 }
+
+                ibLikes.setOnClickListener {
+                    viewModel.likeById(post.id)
+                }
+                ibShares.setOnClickListener {
+                    val intent = Intent().apply {
+                        action = Intent.ACTION_SEND
+                        putExtra(Intent.EXTRA_TEXT, post.content)
+                        type = "text/plain"
+                    }
+                    startActivity(intent)
+                    viewModel.share(post.id)
+                }
+
+                ibPlayVideo.setOnClickListener(playVideoListener)
+                ivThumbnail.setOnClickListener(playVideoListener)
+                ibVideo.setOnClickListener(playVideoListener)
+
+                // слушатель на кнопку "три точки"
+                buttonMenu.setOnClickListener {
+                    PopupMenu(it.context, it).apply {
+                        inflate(R.menu.options_post)
+                        setOnMenuItemClickListener { item ->
+                            when (item.itemId) {
+                                R.id.remove -> {
+                                    viewModel.remove(post.id)
+                                    findNavController().navigateUp()
+                                    true
+                                }
+                                R.id.add -> {
+                                    val epost = Post (
+                                        id = -1L,
+                                        author = it.context.getString(R.string.title),
+                                        content = "",
+                                        published = "",
+                                        likes = 0,
+                                        shares = 0,
+                                        views = 0,
+                                        video = ""
+                                    )
+                                    viewModel.addAndEdit(epost)
+                                    findNavController().navigateUp()
+                                    true
+                                }
+                                R.id.edit -> {
+                                    viewModel.addAndEdit(post)
+                                    findNavController().navigateUp()
+                                    true
+                                }
+                                else -> false
+                            }
+                        }
+                    }.show()
+                }
             }
-
-
         }
         return binding.root
     }
