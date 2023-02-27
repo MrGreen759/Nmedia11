255fe76fc7a45f3a140bbdaa2b467093e1c7f97e
Alexey Zelenkov <alan.lapsky@gmail.com>
2023-02-27 23:24:12 +0300

    27.02.2023 - continue homework "Avatars"

--- app/src/main/java/ru/netology/nmedia11/Adapter.kt
+++ app/src/main/java/ru/netology/nmedia11/Adapter.kt
@@ -6,6 +6,8 @@ import android.view.ViewGroup
 import android.widget.PopupMenu
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.netology.nmedia11.activity.FeedFragment
 import ru.netology.nmedia11.databinding.PostCardBinding
 import ru.netology.nmedia11.utils.Utils
 
@@ -40,6 +42,14 @@ class PostViewHolder(
     fun bind(post: Post) {
         // слушатель для перехода во фрагмент просмотра одного поста
         val bigClick = View.OnClickListener { onInteractionListener.onPost(post.id) }
+
+        val url = "http://10.0.2.2:10999/avatars/${post.authorAvatar}"
+        Glide.with(binding.icon)
+            .load(url)
+            .placeholder(R.drawable.ic_loading_100)
+            .error(R.drawable.ic_error_100)
+            .timeout(10_000)
+            .into(binding.icon)
 
         binding.apply {
             author.text = post.author
